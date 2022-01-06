3583ff1a9c48fa5bb3ecb2968bb058f2ad7b4bee
NagalieArt <70587195+NagalieArt@users.noreply.github.com>
2022-01-06 20:53:35 +0100

    New Laravel project + added views.

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('content')
+<title> Index</title>
+
+<body>
+    <div class="container">   
+        <h2>"Name's" posts</h2>
+        <h3>Recent posts</h3>
+        <h4>Likes</h4>
+    </div>
+
+    <div class="container">   
+        <h2>Posts you liked</h2>
+        <h3>Recent posts</h3>
+        <h4>Likes</h4>
+    </div>
+
+    
+</body>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('content')
+<title> Welcome </title>
+
+<body>
+    <div class="container">   
+        <h1>Welcome to the BlogPoster!</h1>
+        <h3>Here you can write your own blogposts for the world to see!</h3>
+    </div>
+
+    <div class="container">   
+        <h3>IN THE WORKS</h3>
+        <h4>LINKS</h4>
+        <a href="/homepage">homepage</a>
+        <a href="/home">home</a>
+    </div>
+    
+</body>
+
+@endsection
+
